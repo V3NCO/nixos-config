82238b59ad35c897c7558a62e4aef8cad61fc602
Venco <v3nco@v3nco.dev>
2025-08-07 22:10:37 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A template that shows all standard flake outputs";
+
+  # Inputs
+  # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
+
+  inputs = {
+    # Nixpkgs Repo on github at version 25.05
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Home Manager
+    home-manager = { 
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.quasar = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users = {
+              venco = import ./home.nix;
+            };
+          };
+        }
+      ];
+    };
+  };
+}
