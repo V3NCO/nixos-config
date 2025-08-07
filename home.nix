066066d0956fad928bcb025f9e2f8b00cc91bad4
Venco <v3nco@v3nco.dev>
2025-08-07 22:11:11 +0200

    Add signing key

--- home.nix
+++ home.nix
@@ -112,6 +112,7 @@
     userName = "Venco";
     userEmail = "v3nco@v3nco.dev";
     signing.signByDefault = true;
+    signing.key = "6FAA8732F6512AAD6EDC522AE5E0FA4E59297845";
   };
 
   # starship - an customizable prompt for any shell
