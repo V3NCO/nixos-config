fc1b8343c30316544befd59b8727723cf8762067
Venco <v3nco@v3nco.dev>
2025-08-09 16:00:00 +0200

    Laptop basic config

---
+++ systems/comet/default.nix
@@ -0,0 +1,18 @@
+{pkgs, config, inputs, ...}:
+{    
+  imports = [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/nixos/basic
+      ../../modules/nixos/desktop
+      ../../modules/nixos/printing.nix
+      ../../modules/nixos/ssh.nix
+      ../../modules/nixos/xserver.nix
+      ../../modules/nixos/polkit.nix
+      ../../users
+  ];
+
+
+  networking.hostName = "comet";
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "25.05";
+}
