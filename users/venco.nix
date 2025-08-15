c99c6d94ed023548834ffc3aeefa1d32d2be2de4
Venco <v3nco@v3nco.dev>
2025-08-15 14:01:15 +0200

    Clipping try 2

--- users/venco.nix
+++ users/venco.nix
@@ -85,10 +85,15 @@
       playerctl
       killall
       clipse
+      bash
     ];
   };
 
-  programs.gpu-screen-recorder.enable = true;
+  programs.gpu-screen-recorder.enable = true; # For promptless recording on both CLI and GUI
+
+    environment.systemPackages = with pkgs; [
+      gpu-screen-recorder-gtk # GUI app
+    ];
 
   imports = [
     ../modules/nixos/browsers
