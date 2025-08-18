05aa11fbf21c7cd27f32a3e55cd4e90339fbc72b
Venco <v3nco@v3nco.dev>
2025-08-18 17:56:58 +0200

    Updates

--- modules/home-manager/caelestia.nix
+++ modules/home-manager/caelestia.nix
@@ -1,11 +1,19 @@
 { inputs, ... }:
-let
-  caelestia-shell = inputs.caelestia-shell.packages."x86_64-linux".default.override {
-    withCli = true;
-  };
-in
 {
-  home.packages = [
-    caelestia-shell
+  imports = [
+    (import "${inputs.caelestia-shell}/nix/hm-module.nix" inputs.caelestia-shell)
   ];
+
+  programs.caelestia = {
+    enable = true;
+    cli.enable = true;
+
+    settings = {
+      theme = "mocha";
+    };
+
+    cli.settings = {
+      commandDelay = 120;
+    };
+  };
 }
--- users/venco.nix
+++ users/venco.nix
@@ -89,6 +89,7 @@
       clipse
       bash
       godot
+      bitwarden-cli
     ];
   };
 
