07a5648ed8b22381b648db21a373cf963a2c2230
Venco <v3nco@v3nco.dev>
2025-09-02 00:29:31 +0200

    Switch fonts to more global place

--- users/default.nix
+++ users/default.nix
@@ -1,8 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   imports = [
     ./venco.nix
   ];
 
   users.defaultUserShell = pkgs.zsh;
+  fonts.packages = [
+    inputs.apple-fonts.packages.${pkgs.system}.sf-pro
+    inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-compact
+    inputs.apple-fonts.packages.${pkgs.system}.sf-compact-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-mono
+    inputs.apple-fonts.packages.${pkgs.system}.sf-mono-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-arabic
+    inputs.apple-fonts.packages.${pkgs.system}.sf-arabic-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-armenian
+    inputs.apple-fonts.packages.${pkgs.system}.sf-armenian-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-georgian
+    inputs.apple-fonts.packages.${pkgs.system}.sf-georgian-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-hebrew
+    inputs.apple-fonts.packages.${pkgs.system}.sf-hebrew-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.ny
+    inputs.apple-fonts.packages.${pkgs.system}.ny-nerd
+  ];
 }
