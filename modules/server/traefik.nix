0cf81db7f34a18ad270faec0a05895399262f0da
Venco <v3nco@v3nco.dev>
2025-10-23 19:05:05 +0200

    Update traefik.nix

--- modules/server/traefik.nix
+++ modules/server/traefik.nix
@@ -2,7 +2,7 @@
 {
     environment.variables = {
       CF_DNS_API_TOKEN_FILE = "${config.services.traefik.dataDir}/CF_API_TOKEN";
-    }
+    };
     services.traefik = {
       enable = true;
         staticConfigOptions = {
