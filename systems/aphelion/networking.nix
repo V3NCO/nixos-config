3e62ff90128584e846ab0caeb1770941d98a77d5
Venco <v3nco@v3nco.dev>
2025-10-25 22:09:42 +0200

    Update firewall for aphelion

--- systems/aphelion/networking.nix
+++ systems/aphelion/networking.nix
@@ -7,5 +7,13 @@
     interfaces.enp3s0.ipv4.addresses = [
       {address = "45.8.201.68"; prefixLength = 24;}
     ];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 443 ];
+      allowedUDPPortRanges = [
+        { from = 4000; to = 4007; }
+        { from = 8000; to = 8010; }
+      ];
+    };
   };
 }
