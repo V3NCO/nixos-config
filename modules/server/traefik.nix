8615cf24785f49a8bff90f9697dfa2e2e060db15
Venco <v3nco@v3nco.dev>
2025-10-25 17:21:31 +0200

    Update traefik.nix

--- modules/server/traefik.nix
+++ modules/server/traefik.nix
@@ -1,11 +1,13 @@
 { config, ... }:
 {
-    environment.variables = {
+    systemd.services.traefik.environment = {
       CF_DNS_API_TOKEN_FILE = "${config.services.traefik.dataDir}/CF_API_TOKEN";
     };
     services.traefik = {
       enable = true;
         staticConfigOptions = {
+          global.sendAnonymousUsage = false;
+
           entryPoints = {
             web = {
               address = ":80";
@@ -41,6 +43,21 @@
             format = "json";
           };
 
+          accessLog = {
+            filePath = "${config.services.traefik.dataDir}/access.log";
+            format = "json";
+            filters = {
+              statusCodes = [ "200-299" "400-599" ];
+            };
+            bufferingSize = 0;
+            fields.headers = {
+              defaultMode = "drop";
+              names = {
+                User-Agent = "keep";
+              };
+            };
+          };
+
           certificatesResolvers.cfacme.acme = {
             email = "certificates@v3nco.dev";
             storage = "${config.services.traefik.dataDir}/acme.json";
@@ -58,6 +75,45 @@
         dynamicConfigOptions = {
             http.routers = {};
             http.services = {};
+            http.middlewares = {
+              local-ipwhitelist.ipAllowList.sourceRange = [ "192.168.0.0/16" "10.0.0.0/8" "127.0.0.1/32" "172.16.0.0/12" "100.0.0.0/8" ];
+              security-headers.headers = {
+                customResponseHeaders = {
+                  Server = "";
+                  X-Powered-By = "";
+                  X-Forwarded-Proto = "https"
+                };
+                sslProxyHeaders = {
+                  X-Forwarded-Proto = "https";
+                };
+                hostsProxyHeaders = [ "X-Forwarded-For" ]
+                contentTypeNosniff = true;
+                customFrameOptionsValue = "SAMEORIGIN";
+                browserXssFilter = false;
+                referrerPolicy = "strict-origin-when-cross-origin"
+                forceSTSHeader = true;
+                stsIncludeSubdomains = true;
+                stsSeconds = 63072000;
+                stsPreload = true;
+              };
+              rate-limit.rateLimit = {
+                average = 100;
+                burst = 100;
+                period = 1;
+              };
+              basic-auth.basicAuth.usersFile = "${config.services.traefik.dataDir}/admindash.htpasswd";
+            tls.options.default = {
+              sniStrict = true;
+              minVersion = "VersionTLS12";
+              cipherSuites = [
+                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+                "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+                "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+                "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+              ];
+            };
         };
     };
 }
