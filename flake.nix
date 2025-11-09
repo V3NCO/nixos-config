130cab009dedb3f4cccc64983b3f7df33cb238ba
Venco <v3nco@v3nco.dev>
2025-11-09 17:58:36 +0100

    Hyprland cachix take 2

--- flake.nix
+++ flake.nix
@@ -3,7 +3,12 @@
 
   # Inputs
   # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
-
+  nixConfig = {
+    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -6,11 +6,5 @@
     ./hyprland.nix
   ];
 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   environment.systemPackages = [ pkgs.kdePackages.full ];
 }
