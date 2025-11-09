2e3d5d6125b1aec2ac189c02795d801c4dab7329
Venco <v3nco@v3nco.dev>
2025-11-09 17:29:46 +0100

    Hyprland cachix take 1

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -5,5 +5,12 @@
     ./greet.nix
     ./hyprland.nix
   ];
+
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   environment.systemPackages = [ pkgs.kdePackages.full ];
 }
