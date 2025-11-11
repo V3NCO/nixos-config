c1c032066487d6322483c1e7da5975f1bf63deed
Venco <v3nco@v3nco.dev>
2025-11-11 15:29:16 +0100

    Other configs

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -25,10 +25,47 @@
     ];
     settings = {
       "$mod" = "SUPER";
+      exec = [ "hyprctl dispatch submap global" ];
+      submap = "global";
+      general.layout = "scrolling";
+      cursor.no_hardware_cursors = true;
+
       bind = [
-        "$mod, T, exec, kitty"
+        "$mod, SPACE, global, caelestia:launcher"
+        "$mod, RETURN, exec, kitty"
+        "$mod, Q, killactive"
         "$mod, L, exit"
+
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        "$mod SHIFT, mouse_up, layoutmsg, move +col"
+        "$mod SHIFT, mouse_down, layoutmsg, move -col"
       ];
+      binde = [
+        "$mod, parenright, splitratio, -0.1"
+        "$mod, Equal, splitratio, 0.1"
+      ];
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, X, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod, X, resizewindow"
+      ];
+      bindin = [
+        "$mod, catchall, global, caelestia:launcherInterrupt"
+        "$mod, mouse:272, global, caelestia:launcherInterrupt"
+        "$mod, mouse:273, global, caelestia:launcherInterrupt"
+        "$mod, mouse:274, global, caelestia:launcherInterrupt"
+        "$mod, mouse:275, global, caelestia:launcherInterrupt"
+        "$mod, mouse:276, global, caelestia:launcherInterrupt"
+        "$mod, mouse:277, global, caelestia:launcherInterrupt"
+        "$mod, mouse_up, global, caelestia:launcherInterrupt"
+        "$mod, mouse_down, global, caelestia:launcherInterrupt"
+      ];
+
       input = {
         kb_layout = "fr";
         numlock_by_default = true;
@@ -36,8 +73,23 @@
         repeat_rate = 35;
         follow_mouse = 1;
         off_window_axis_events = 2;
-
       };
+
+      exec-once = [
+        "gnome-keyring-daemon --start --components=secrets"
+        "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+        "clipse -listen"
+        "caelestia shell -d"
+      ];
+
+      plugin = {
+        hyprscrolling = {
+          column_width = 0.9;
+          fullscreen_on_one_column = false;
+          focus_fit_method = 1;
+        };
+      };
+
     };
   };
 }
