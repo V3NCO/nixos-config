eb4d656440123026bd9aa5dd981cb88a3a661504
Venco <v3nco@v3nco.dev>
2025-11-11 17:08:14 +0100

    144Hz

--- systems/quasar/hyprland/monitors.nix
+++ systems/quasar/hyprland/monitors.nix
@@ -3,7 +3,7 @@
   programs.hyprland.settings = {
     monitor = [
       "DP-1, 1920x1080, 1920x0, 1"
-      "HDMI-A-2, 1920x1080, 0x0, 1"
+      "HDMI-A-2, 1920x1080, 0x0@144, 1"
       "DP-2, 1920x1080, -1920x0, 1"
     ];
   };
