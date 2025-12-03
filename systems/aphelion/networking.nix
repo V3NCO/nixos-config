dfcc590939f8221900f5dc2133f52c0a7b55b995
Venco <v3nco@v3nco.dev>
2025-12-03 18:08:42 +0100

    Update networking.nix

--- systems/aphelion/networking.nix
+++ systems/aphelion/networking.nix
@@ -1,18 +1,18 @@
 { ... }:
 {
   networking = {
-    defaultGateway = "45.8.201.1";
+    #defaultGateway = "45.8.201.1";
     hostName = "aphelion";
     nameservers = [
       "8.8.8.8"
       "1.1.1.1"
     ];
-    interfaces.enp3s0.ipv4.addresses = [
-      {
-        address = "45.8.201.68";
-        prefixLength = 24;
-      }
-    ];
+    #interfaces.enp3s0.ipv4.addresses = [
+    #  {
+    #    address = "45.8.201.68";
+    #    prefixLength = 24;
+    #  }
+    #];
     firewall = {
       enable = true;
       allowedTCPPorts = [
