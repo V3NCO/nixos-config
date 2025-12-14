f19191cd09cd46d1e95ddedde73b9ae337d8ce45
Venco <v3nco@v3nco.dev>
2025-12-14 01:47:29 +0100

    aaaa

--- users/venco-server.nix
+++ users/venco-server.nix
@@ -2,7 +2,7 @@
 {
   users.users.venco = {
     isNormalUser = true;
-    initialPassword = "venco";
+    initialPassword = "password";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
