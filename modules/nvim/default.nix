82ad76959360cb651bd4fe30c55ce38809e6805f
Venco <v3nco@v3nco.dev>
2026-01-02 04:26:19 +0100

    nixcats

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -4,33 +4,82 @@
     enable = true;
     packageNames = [ "nvim" ];
 
-    # Point to your lua config directory
-    luaPath = "."; # or wherever your init.lua, plugin/, ftplugin/ are
+    luaPath = ./.; # Points to modules/nvim/ where init.lua lives
 
-    # Define your categories (plugins, LSPs, etc.)
-    categoryDefinitions =
-      { pkgs, ... }:
+    categoryDefinitions.replace =
+      {
+        pkgs,
+        settings,
+        categories,
+        extra,
+        name,
+        mkPlugin,
+        ...
+      }@packageDef:
       {
         lspsAndRuntimeDeps = {
           general = with pkgs; [
+            lazygit
             lua-language-server
-            nixd
             stylua
+            nixd
+            alejandra
           ];
         };
+
         startupPlugins = {
           general = with pkgs.vimPlugins; [
+            # UI & Core
+            snacks-nvim
+            onedark-nvim
+            vim-sleuth
+            mini-ai
+            mini-icons
+            mini-pairs
+
+            # LSP
             nvim-lspconfig
+
+            # Completion
+            blink-cmp
+
+            # Treesitter
             nvim-treesitter.withAllGrammars
-            # ... your plugins
+
+            # Status line
+            lualine-nvim
+            lualine-lsp-progress
+
+            # Git
+            gitsigns-nvim
+
+            # Keybindings
+            which-key-nvim
+
+            # Linting & Formatting
+            nvim-lint
+            conform-nvim
+
+            # Debugging
+            nvim-dap
+            nvim-dap-ui
+            nvim-dap-virtual-text
+
+            # Startup time profiling
+            vim-startuptime
+          ];
+        };
+
+        optionalPlugins = {
+          general = with pkgs.vimPlugins; [
+            lazydev-nvim
           ];
         };
       };
 
-    # Define your package
-    packageDefinitions = {
+    packageDefinitions.replace = {
       nvim =
-        { pkgs, ... }:
+        { pkgs, name, ... }:
         {
           settings = {
             suffix-path = true;
