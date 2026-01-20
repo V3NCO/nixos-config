8daf274bdefd8deff45520b79bd1fc2752f37732
Venco <v3nco@v3nco.dev>
2026-01-20 01:06:48 +0100

    stardust config

---
+++ systems/stardust/default.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./apple-silicon-support
+    ../../modules/nvim
+    ../../modules/nixos/basic
+    ../../modules/nixos/basic/networking.nix
+    ../../modules/nixos/basic/bluetooth.nix
+    ../../modules/nixos/desktop
+    ../../modules/nixos/printing.nix
+    ../../modules/nixos/ssh.nix
+    ../../modules/nixos/xserver.nix
+    ../../modules/nixos/polkit.nix
+    ../../modules/nixos/drawing_tablets.nix
+    ../../modules/nixos/theming/catppuccin.nix
+    ../../modules/nixos/music-making.nix
+    ../../modules/nixos/flipperzero.nix
+    ../../modules/nixos/cider.nix
+    ../../users
+    ./keyboard.nix
+  ];
+
+  virtualisation.docker = {
+    enable = true;
+  };
+  services.flatpak.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ 8000 ];
+  networking.firewall.allowedUDPPorts = [ 8000 ];
+  networking.nameservers = ["1.1.1.1" "9.9.9.9"];
+  time.timeZone = "Europe/Paris";
+  networking.networkmanager.enable = true;
+  networking.wireless.iwd = {
+    enable = true;
+    settings.General.EnableNetworkConfiguration = true;
+  };
+
+
+  programs.nix-ld.enable = true;
+  networking.hostName = "stardust";
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "25.11";
+  hardware.ledger.enable = true;
+}
---
+++ systems/stardust/keyboard.nix
@@ -0,0 +1,26 @@
+{
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "mac";
+  };
+
+  console.keyMap = "fr-mac";
+
+  programs.hyprland.settings.input.kb_layout = "fr";
+
+  programs.dconf = {
+    enable = true;
+    profiles = {
+      user.databases = [
+        {
+          settings = {
+            "org/gnome/desktop/input-sources" = {
+              sources = [ "('xkb', 'fr+mac')" ];
+              xkb-options = [ "terminate:ctrl_alt_bksp" ];
+            };
+          };
+        }
+      ];
+    };
+  };
+}
