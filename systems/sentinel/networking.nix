ed94f199a24eb501e1aa525ccf017eccbdfe8ec9
Venco <v3nco@v3nco.dev>
2026-03-13 14:36:06 +0100

    So i think the host ip is gonna be 192.168.0.221 and then the rest of the vms will get 192.168.0.222, 192.168.0.223.... and hopefully its just a matter of setting the bridge as the network interface for each vm

--- systems/sentinel/networking.nix
+++ systems/sentinel/networking.nix
@@ -3,6 +3,7 @@
   networking = {
     networkmanager.enable = lib.mkForce false;
 
+    networking.defaultGateway = "10.0.0.1";
     useNetworkd = true;
     nftables.enable = true;
 
@@ -10,10 +11,10 @@
       interfaces = [ "enp86s0" ];
     };
 
-    interfaces.br0 = {
-      useDHCP = true;
-    };
-    
+    networking.interfaces.br0.ipv4.addresses = [
+        { address = "192.168.0.221"; prefixLength = 24; }
+      ];
+
     firewall = {
       enable = true;
       allowedTCPPorts = [
