599829c49e14e1cba9fe5c9d917fae6fe12f1596
Venco <v3nco@v3nco.dev>
2026-03-15 21:28:49 +0100

    SDDM except it looks good

--- modules/nixos/desktop/greet.nix
+++ modules/nixos/desktop/greet.nix
@@ -2,15 +2,36 @@
 {
   imports = [inputs.silentSDDM.nixosModules.default];
   environment.systemPackages = [];
+  services.displayManager.defaultSession = "niri";
+  services.xserver.displayManager.setupCommands = "";
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true;
+    wayland = {
+      enable = true;
+      compositor = "kwin";
+    };
     autoNumlock = true;
     package = pkgs.kdePackages.sddm;
   };
 
   programs.silentSDDM = {
     enable = true;
-    theme = "rei";
+    theme = "default";
+    profileIcons = {
+      venco = ../../../users/pfp/venco.png;
+    };
+
+    backgrounds = {
+      sakura = ../../home-manager/desktop/wallpapers/flowers-20.jpg;
+    };
+
+    settings = {
+      "LockScreen" = {
+        background = "flowers-20.jpg";
+      };
+      "LoginScreen" = {
+        background = "flowers-20.jpg";
+      };
+    };
   };
 }
