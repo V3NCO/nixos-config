b4f2ba32b35dc8fcd9d29f6222e6023f3ffb7feb
Venco <v3nco@v3nco.dev>
2026-03-15 03:13:28 +0100

    add silentsddm

--- modules/nixos/desktop/greet.nix
+++ modules/nixos/desktop/greet.nix
@@ -1,10 +1,16 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
+  imports = [inputs.silentSDDM.nixosModules.default];
   environment.systemPackages = [];
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     autoNumlock = true;
     package = pkgs.kdePackages.sddm;
+  };
+
+  programs.silentSDDM = {
+    enable = true;
+    theme = "rei";
   };
 }
