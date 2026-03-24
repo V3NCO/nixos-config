7f22557150e0ff3322bdb371c2f85488e130c5c6
Venco <v3nco@v3nco.dev>
2026-03-25 00:35:15 +0100

    Update radicale.nix

--- modules/server/radicale.nix
+++ modules/server/radicale.nix
@@ -1,12 +1,13 @@
 { ... }:
 {
-homelab.services.radicale = {
-  subdomain = "radicale";
-  zone = "v3nco";
-  upstream = {
-    scheme = "http";
-    host = "127.0.0.1";
-    port = 5232;
+  homelab.services.radicale = {
+    subdomain = "radicale";
+    zone = "v3nco";
+    upstream = {
+      scheme = "http";
+      host = "127.0.0.1";
+      port = 5232;
+    };
   };
 
   homelab.ports = [ 5232 ];
@@ -25,5 +26,5 @@ homelab.services.radicale = {
         filesystem_folder = "/var/lib/radicale/collections";
       };
     };
-  }
+  };
 }
