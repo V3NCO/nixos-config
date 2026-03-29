cf4b7dce74fba4e1bbd48f9e973c1415431323e6
Venco <v3nco@v3nco.dev>
2026-03-30 00:41:08 +0200

    try ts

--- systems/sentinel/backups.nix
+++ systems/sentinel/backups.nix
@@ -1,3 +1,49 @@
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    restic
+  ];
+
+
+  services.restic = {
+    backups = let
+      initialize = true;
+      exclude = [
+        "/home/*/.cache"
+        "*.log"
+      ];
+      paths = [
+        "/var/lib"
+        "/etc"
+        "/home"
+        "/root"
+      ];
+      checkOpts = [
+        "--with-cache"
+      ];
+      extraBackupArgs = [
+      ];
+      extraOptions = [
+      ];
+      passwordFile = "/var/lib/restic/password";
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+        "--keep-yearly 10"
+      ];
+      timerConfig = {
+        OnBootSec = "3m";
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+    in {
+      backup = {
+        inherit initialize exclude paths timerConfig checkOpts extraBackupArgs extraOptions passwordFile pruneOpts;
+
+        environmentFile = "/var/lib/restic/.env";
+        repository = "sftp:sentinel@100.89.108.16:/Backup-Sentinel/restic";
+      };
+    };
+  };
 }
