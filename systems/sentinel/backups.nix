5779d713f8c9670846360d0efe05d7117471cd31
Venco <v3nco@v3nco.dev>
2026-04-06 22:44:57 +0200

    Update backups.nix

--- systems/sentinel/backups.nix
+++ systems/sentinel/backups.nix
@@ -11,7 +11,7 @@
         options = let
           automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-        in &#91;"${automount_opts},credentials=/etc/nixos/smb-secrets"];
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
     };
 
   services.restic = {
