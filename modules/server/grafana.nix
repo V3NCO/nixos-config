0dadb701a3a9bcd9781a6e993baad3e7078938b8
Venco <v3nco@v3nco.dev>
2026-04-08 19:46:21 +0200

    well beszel is not very declarative

---
+++ modules/server/grafana.nix
@@ -0,0 +1,53 @@
+{ ... }:
+{
+  homelab = {
+    ports = [ config.services.grafana.settings.server.http_port 9090 ];
+    services.grafana = {
+      subdomain = "grafana";
+      zone = "v3nco";
+      upstream = {
+        scheme = "http";
+        host = "127.0.0.1";
+        port = config.services.grafana.settings.server.http_port;
+      };
+      middlewares = [
+        "security-headers"
+      ];
+    };
+  };
+
+  services.grafana = {
+    enable = true;
+    settings.server.http_port = 9349;
+
+    provision = {
+      enable = true;
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          access = "proxy";
+          url = "http://127.0.0.1:9090";
+        }
+      ];
+      dashboards.settings.providers = [
+        {
+          name = "My Dashboards";
+          options.path = "/etc/grafana-dashboards";
+        }
+      ];
+    };
+  };
+
+  services.prometheus = {
+    enable = true;
+    exporters.node.enable = true;
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [{ targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ]; }];
+      }
+    ];
+  };
+
+}
