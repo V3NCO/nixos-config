f40ed6106906b84fdd8518bd0e4c1f7852e030ee
Venco <v3nco@v3nco.dev>
2026-04-09 17:29:59 +0200

    lidarr
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/server/music/default.nix
+++ modules/server/music/default.nix
@@ -2,5 +2,6 @@
 {
   imports = [
     ./navidrome.nix
+    ./lidarr.nix
   ];
 }
---
+++ modules/server/music/lidarr.nix
@@ -0,0 +1,32 @@
+{ config, ... }:
+{
+  homelab = {
+    ports = [ config.services.lidarr.settings.server.port ];
+    services.lidarr = {
+      subdomain = "lidarr";
+      zone = "v3nco";
+      upstream = {
+        scheme = "http";
+        host = "127.0.0.1";
+        port = config.services.lidarr.settings.server.port;
+      };
+      middlewares = [
+        "security-headers"
+      ];
+    };
+  };
+
+  services.lidarr = {
+    enable = true;
+    group = "music";
+    settings = {
+      server = {
+        urlbase = "https://lidarr.v3nco.dev";
+        port = 8686;
+      };
+    };
+  };
+
+  users.groups.music = {};
+  users.users.lidarr.group = "music";
+}
