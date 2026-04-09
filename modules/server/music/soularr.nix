90c2c4a3c25916d0735a6e4642257997ee1de06b
Venco <v3nco@v3nco.dev>
2026-04-09 20:53:55 +0200

    Create soularr.nix
    Signed-off-by: Venco <v3nco@v3nco.dev>

---
+++ modules/server/music/soularr.nix
@@ -0,0 +1,35 @@
+{ config, ... }:
+{
+  homelab = {
+    ports = [ config.services.lidarr.settings.web.port config.services.slskd.settings.soulseek.listen_port ];
+    services.slskd = {
+      subdomain = "slskd";
+      zone = "v3nco";
+      upstream = {
+        scheme = "http";
+        host = "127.0.0.1";
+        port = config.services.slskd.settings.web.port;
+      };
+      middlewares = [
+        "security-headers"
+      ];
+    };
+  };
+
+  services.slskd = {
+    enable = true;
+    openFirewall = true;
+    group = "music";
+    environmentFile = "/var/lib/slskd/.env";
+    settings = {
+      directories = {
+        incomplete = "/shared/downloads/incomplete/music";
+        downloads = "/shared/downloads/music";
+      };
+      shares.directories = [
+        "/shared/music"
+      ];
+      remote_file_management = true;
+    };
+  };
+}
