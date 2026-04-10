0c40cf6671048b718b588d14dafb7737a7f69ac8
Venco <v3nco@v3nco.dev>
2026-04-10 22:26:17 +0200

    Update soulseek.nix
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/server/music/soulseek.nix
+++ modules/server/music/soulseek.nix
@@ -1,33 +1,4 @@
-{ pkgs, inputs, config, ... }:
-
-let
-
-  unstable = import inputs.nixpkgs-unstable {
-    system = pkgs.stdenv.hostPlatform.system;
-    config.allowUnfree = true;
-  };
-
-  wrtag = (unstable.buildGoModule (finalAttrs: {
-    pname = "wrtag";
-    version = "0.30.0";
-
-    src = unstable.fetchFromGitHub {
-      owner = "sentriz";
-      repo = "wrtag";
-      tag = "v${finalAttrs.version}";
-      hash = "sha256-ba3HrAUaI9onuRFns9q2fkJxZWhadqJjd8rAmlIVvg4=";
-    };
-
-    vendorHash = "sha256-S0emGAQJi9MLvyU3lL/Vrc4SZ10w6MOqND0LsBI7lg8=";
-
-    nativeBuildInputs = [ unstable.installShellFiles ];
-    buildInputs = [ unstable.ffmpeg unstable.chromaprint unstable.rsgain ];
-    postInstall = ''
-      installShellCompletion contrib/completions/wrtag.{fish,bash}
-      installShellCompletion contrib/completions/metadata.fish
-    '';
-  }));
-in
+{ pkgs, config, ... }:
 {
   # Just go in the git history for the beets config ngl
 
@@ -46,48 +17,15 @@ in
           "security-headers"
         ];
       };
-
-      wrtag = {
-        subdomain = "wrtag";
-        zone = "v3nco";
-        upstream = {
-          scheme = "http";
-          host = "127.0.0.1";
-          port = 7834;
-        };
-        middlewares = [
-          "security-headers"
-        ];
-      };
     };
   };
 
   environment.systemPackages = [
     pkgs.ffmpeg
     pkgs.chromaprint
-    wrtag
   ];
 
 
-  systemd.services."wrtag-web" = {
-    serviceConfig = {
-      ExecStart = ''
-        ${wrtag}/bin/wrtagweb -addon "lyrics lrclib musixmatch genius" -addon "replaygain"
-      '';
-      User = "wrtag";
-      Group = "music";
-      EnvironmentFile = "/var/lib/wrtag/.env";
-    };
-    environment = {
-      PATH = pkgs.lib.mkForce (pkgs.lib.makeBinPath [ unstable.ffmpeg unstable.chromaprint unstable.rsgain ]);
-      WRTAG_WEB_DB_PATH = "/var/lib/wrtag/wrtag.db";
-      WRTAG_WEB_LISTEN_ADDR = ":7834";
-      WRTAG_WEB_PUBLIC_URL = "https://wrtag.v3nco.dev";
-      WRTAG_PATH_FORMAT = ''
-        /shared/music/{{ artists .Release.Artists | sort | join "; " | safepath }}/({{ .Release.ReleaseGroup.FirstReleaseDate.Year }}) {{ .Release.Title | safepath }}{{ if not (eq .ReleaseDisambiguation "") }} ({{ .ReleaseDisambiguation | safepath }}){{ end }}/{{ if gt (len .Release.Media) 1 }}d{{ pad0 2 .Media.Position }} {{ end }}{{ pad0 2 .Track.Position }}.{{ .Media.TrackCount | pad0 2 }} {{ if .IsCompilation }}{{ artistsString .Track.Artists | safepath }} - {{ end }}{{ .Track.Title | safepath }}{{ .Ext }}
-      '';
-    };
-  };
 
   users.groups.music = {};
   users.users.wrtag = { group = "music"; isSystemUser = true; };
