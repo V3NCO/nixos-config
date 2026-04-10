aab18bc8af569f635c68a578c4519d77fe193326
Venco <v3nco@v3nco.dev>
2026-04-10 14:11:07 +0200

    beeets service
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/server/music/soulseek.nix
+++ modules/server/music/soulseek.nix
@@ -1,4 +1,39 @@
 { pkgs, lib, config, ... }:
+
+let
+  customBeets = (pkgs.python313Packages.beets.override {
+    pluginOverrides = {
+      beetcamp = {
+        enable = true;
+        propagatedBuildInputs = [ pkgs.python313Packages.beetcamp ];
+      };
+      youtube = {
+        enable = true;
+        propagatedBuildInputs = [
+          (pkgs.python313Packages.buildPythonPackage rec {
+            pname = "beets-youtube";
+            version = "0.1.0";
+            src = pkgs.fetchFromGitHub {
+              owner = "arsaboo";
+              repo = "beets-youtube";
+              rev = "85503871a901c24220214c66ce22ac191eb0417c";
+              hash = "sha256-n5wssgsNZOXNcX2fhORPtCozIqTUSXHfL0EQ8gbsN3k=";
+            };
+            format = "setuptools";
+            propagatedBuildInputs = with pkgs.python313Packages; [
+              ytmusicapi
+              requests
+              pillow
+            ];
+          })
+        ];
+      };
+    };
+  }).overridePythonAttrs (old: {
+    catchConflicts = false;
+  });
+
+in
 {
   homelab = {
     ports = [ config.services.slskd.settings.web.port config.services.slskd.settings.soulseek.listen_port ];
@@ -17,38 +52,7 @@
   };
 
   environment.systemPackages = [
-    ((pkgs.python313Packages.beets.override {
-      pluginOverrides = {
-        bandcamp = {
-          enable = true;
-          propagatedBuildInputs = [ pkgs.python313Packages.beetcamp ];
-        };
-        youtube = {
-          enable = true;
-          propagatedBuildInputs = [
-            (pkgs.python313Packages.buildPythonPackage rec {
-              pname = "beets-youtube";
-              version = "0.1.0";
-              src = pkgs.fetchFromGitHub {
-                owner = "arsaboo";
-                repo = "beets-youtube";
-                rev = "85503871a901c24220214c66ce22ac191eb0417c";
-                hash = "sha256-n5wssgsNZOXNcX2fhORPtCozIqTUSXHfL0EQ8gbsN3k=";
-              };
-              format = "setuptools";
-              propagatedBuildInputs = with pkgs.python313Packages; [
-                ytmusicapi
-                requests
-                pillow
-              ];
-            })
-          ];
-        };
-      };
-    }).overridePythonAttrs (old: {
-      catchConflicts = false;
-    }))
-
+    customBeets
     pkgs.ffmpeg
     pkgs.chromaprint
   ];
@@ -63,7 +67,7 @@
   };
 
   systemd.services."beets-import-soulseek" = {
-    script = "set -eu && ${lib.getExe pkgs.beets} import /shared/downloads/music --quiet";
+    script = "set -eu && ${lib.getExe customBeets} import /shared/downloads/music --quiet";
     serviceConfig = {
       Type = "oneshot";
       User = "root";
