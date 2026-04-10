4b22f7c00a501d30465ad0665db521385afbe68d
Venco <v3nco@v3nco.dev>
2026-04-10 19:59:30 +0200

    Update soulseek.nix
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/server/music/soulseek.nix
+++ modules/server/music/soulseek.nix
@@ -14,9 +14,7 @@ let
 
     vendorHash = "sha256-CevWYD93fdt7MmWZjBKGR3+isOzWzAo5c8X55qG8/2A=";
 
-    go = pkgs.go_1_26;
-
-    nativeBuildInputs = [ pkgs.installShellFiles ];
+    nativeBuildInputs = [ pkgs.installShellFiles pkgs.go_1_26 ];
 
     postInstall = ''
       installShellCompletion contrib/completions/wrtag.{fish,bash}
