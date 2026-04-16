4cebdce4893189922b7a7198533a5914b80454b6
Venco <v3nco@v3nco.dev>
2026-04-16 23:14:07 +0200

    Update nextcloud.nix
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -17,6 +17,7 @@
     enable = true;
     package = pkgs.nextcloud33;
     extraAppsEnable = true;
+    config.adminpassFile = "/var/lib/adminPass";
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
     };
