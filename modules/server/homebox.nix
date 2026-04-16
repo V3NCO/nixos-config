78f041baa770ff70f17c88835a5a4a6c7198e064
Venco <v3nco@v3nco.dev>
2026-04-17 00:54:23 +0200

    Create homebox.nix
    Signed-off-by: Venco <v3nco@v3nco.dev>

---
+++ modules/server/homebox.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+{
+  homelab.services.homebox = {
+    subdomain = "homebox";
+    zone = "v3nco";
+    upstream = {
+      scheme = "http";
+      host = "127.0.0.1";
+      port = config.services.homebox.settings.HBOX_WEB_PORT;
+    };
+    middlewares = [ "security-headers" ];
+  };
+
+  homelab.ports = [ config.services.homebox.settings.HBOX_WEB_PORT ];
+
+  services.homebox = {
+    enable = true;
+    database.createLocally = true;
+    settings = {
+      HBOX_WEB_PORT = 7745;
+      HBOX_STORAGE_CONN_STRING = "file:///var/lib/homebox";
+      HBOX_STORAGE_PREFIX_PATH = "data";
+      HBOX_OPTIONS_ALLOW_REGISTRATION = "false";
+      HBOX_OPTIONS_CHECK_GITHUB_RELEASE = "false";
+      HBOX_MODE = "production";
+      HOME = "/var/lib/homebox";
+      TMPDIR = "/var/lib/homebox/tmp";
+    };
+  };
+
+}
