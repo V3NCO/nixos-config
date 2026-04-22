c7a66683c8d81aa0aa665e87eda64de709b76435
Venco <v3nco@v3nco.dev>
2026-04-22 17:06:24 +0200

    Update services.nix

--- systems/sentinel/services.nix
+++ systems/sentinel/services.nix
@@ -16,4 +16,16 @@
     ../../modules/server/grafana
     ../../modules/server/music
   ];
+
+  system.activationScripts.shared-acl = {
+    text = ''
+      chown syncthing:music /shared || true
+      chmod 2770 /shared || true
+      setfacl -R -m g:music:rwx /shared || true
+      setfacl -R -m m::rwx /shared || true
+      setfacl -R -d -m g:music:rwx /shared || true
+      setfacl -R -d -m m::rwx /shared || true
+      find /shared -type d -exec chmod g+s {} \; || true
+    '';
+  };
 }
