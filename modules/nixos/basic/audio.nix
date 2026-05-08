ec5c114e00f9aa6ff2f18641669f4a19dae1c49c
Venco <v3nco@v3nco.dev>
2026-05-08 13:58:56 +0200

    fix slack being annoying
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/nixos/basic/audio.nix
+++ modules/nixos/basic/audio.nix
@@ -12,6 +12,52 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-restrict-mic" = {
+          "access.rules" = [
+            {
+              "matches" = [
+                {
+                  "application.process.binary" = "slack";
+                }
+              ];
+              "actions" = {
+                "update-props" = {
+                  "default_permissions" = "rx";
+                };
+              };
+            }
+            {
+              "matches" = [
+                {
+                  "application.process.binary" = "electron";
+                }
+              ];
+              "actions" = {
+                "update-props" = {
+                  "default_permissions" = "rx";
+                };
+              };
+            }
+            {
+              "matches" = [
+                {
+                  "application.process.binary" = "chromium";
+                }
+              ];
+              "actions" = {
+                "update-props" = {
+                  "default_permissions" = "rx";
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
   };
   environment.systemPackages = [pkgs.pavucontrol];
 }
