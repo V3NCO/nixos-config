521e22d0ec490b15b163a2900cad42dcee3d2f90
Venco <v3nco@v3nco.dev>
2026-05-15 21:39:47 +0200

    new fonts
    Signed-off-by: Venco <v3nco@v3nco.dev>

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,22 +1,24 @@
 { pkgs, inputs, ... }:
 {
   fonts.packages = [
+    pkgs.corefonts
+    pkgs.vista-fonts
     inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-pro
-  #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-pro-nerd
-  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-compact
-  #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-compact-nerd
-  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-mono
-  #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-mono-nerd
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-arabic
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-arabic-nerd
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-armenian
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-armenian-nerd
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-georgian
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-georgian-nerd
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-hebrew
-  #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-hebrew-nerd
-  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.ny
-  #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.ny-nerd
+    #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-pro-nerd
+    inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-compact
+    #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-compact-nerd
+    inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-mono
+    #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-mono-nerd
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-arabic
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-arabic-nerd
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-armenian
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-armenian-nerd
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-georgian
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-georgian-nerd
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-hebrew
+    #   inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.sf-hebrew-nerd
+    inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.ny
+    #  inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system}.ny-nerd
     pkgs.nerd-fonts.lilex
   ];
 }
