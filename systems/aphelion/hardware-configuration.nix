b02c9bb2f34ca9d2d3b6b534871c5b4e86a29650
Venco <v3nco@v3nco.dev>
2026-07-31 17:00:41 +0200

    new server

--- systems/aphelion/hardware-configuration.nix
+++ systems/aphelion/hardware-configuration.nix
@@ -9,33 +9,28 @@
   ];
 
   boot.initrd.availableKernelModules = [
-    "ahci"
+    "ata_piix"
+    "uhci_hcd"
     "virtio_pci"
-    "virtio_scsi"
-    "xhci_pci"
-    "sd_mod"
     "sr_mod"
     "virtio_blk"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXROOT";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/NIXBOOT";
-    fsType = "ext4";
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
   };
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
   services.qemuGuest.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
